380dffd13aabceb1fd7fbca956fb05355c813db2
MATRUNI <h.uyadav2004@gmail.com>
2025-11-17 14:36:18 +0530

    made links for repo, which unfortunately are not working for some reason

--- logic.js
+++ logic.js
@@ -1,11 +1,17 @@
 let liveLinks=["./Project-1_Color_Change/index.html","./Project-2_BMI_Calculator/index.html","./Project-3_Words/index.html"];
-let repoLinks=[];
+let repoLinks=["https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-1_Color_Change","https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-2_BMI_Calculator"];
 
 let container=document.getElementById("container");
 console.log(container.children.length);
 
 container.addEventListener("click",e=> toLiveLink(Array.from(container.children).indexOf(e.target)) )
 
+container.addEventListener('click', e=>{
+    if(e.target.tagName === "i")
+    {
+        toRepo(indexOf(e.target));
+    }
+});
 function toLiveLink(index)
 {
     window.location.href=(liveLinks[index]);
@@ -13,5 +19,5 @@ function toLiveLink(index)
 
 function toRepo(index)
 {
-    window.location.href=(repoLinks[index]);
+    window.open(repoLinks[index]);
 }
