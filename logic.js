c4084fc107ac999b24c9c52d9f8851483b7776c5
MATRUNI <h.uyadav2004@gmail.com>
2025-11-17 15:33:51 +0530

    now the repo links and live links are working properly

--- logic.js
+++ logic.js
@@ -1,17 +1,22 @@
 let liveLinks=["./Project-1_Color_Change/index.html","./Project-2_BMI_Calculator/index.html","./Project-3_Words/index.html"];
-let repoLinks=["https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-1_Color_Change","https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-2_BMI_Calculator"];
+let repoLinks=["https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-1_Color_Change","https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-2_BMI_Calculator","https://github.com/MATRUNI/FUN-PROJECTS-JS/tree/master/Project-3_Words"];
 
 let container=document.getElementById("container");
 console.log(container.children.length);
 
-container.addEventListener("click",e=> toLiveLink(Array.from(container.children).indexOf(e.target)) )
-
-container.addEventListener('click', e=>{
-    if(e.target.tagName === "i")
+container.addEventListener("click",e=>{
+    let card=e.target.closest(".card");
+    if(e.target.classList.contains("fa-square-github"))
     {
-        toRepo(indexOf(e.target));
+        console.log("ICON Clicked!!")
+        let card=e.target.closest(".card")
+        toRepo(Array.from(container.children).indexOf(card));
+        return;
     }
+    if(!card) return;
+    toLiveLink(Array.from(container.children).indexOf(card));
 });
+
 function toLiveLink(index)
 {
     window.location.href=(liveLinks[index]);
