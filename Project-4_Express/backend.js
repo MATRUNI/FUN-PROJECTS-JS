a74ad82448e77e397c1cea96ec1f0e3d1ee5da49
MATRUNI <h.uyadav2004@gmail.com>
2025-11-20 23:13:54 +0530

    backend for my other project

---
+++ Project-4_Express/backend.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app=express();
+
+app.use(express.json());
+
+const home=require("os").homedir();
+
+const fs=require("fs");
+
+app.get("/home", (req, res)=>
+{
+    fs.readdir(home, (err, fileNames)=>
+    {
+        if(err)
+            throw err;
+        res.json(filterDot(fileNames))
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("Server Running on port: 3000");
+    console.log("http://localhost:3000");
+})
+
+function filterDot(item)
+{
+    return item.filter(e=>{
+        if(e[0]!=".")
+            return e;
+    });
+}
