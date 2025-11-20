1495a77c1ca7f1cb7cea3bf6e447cd4dbe450008
MATRUNI <h.uyadav2004@gmail.com>
2025-11-21 04:17:46 +0530

    made few ungrades

--- Project-4_Express/backend.js
+++ Project-4_Express/backend.js
@@ -4,9 +4,10 @@ const cors= require("cors");
 const app=express();
 
 app.use(cors());
+app.use(express.json());
 
 const home=require("os").homedir();
-
+let pwd=home;
 const fs=require("fs");
 
 app.get("/home", (req, res)=>
@@ -17,7 +18,29 @@ app.get("/home", (req, res)=>
             throw err;
         res.json(filterDot(fileNames))
     })
+});
+
+app.post("", (req, res)=>{
+    console.log("BODY: ", req.body);
+    pwd+="/"+req.body.body;
+    console.log(pwd)
+    res.json({
+        message: "Accepted",
+        data: req.body
+    });
 })
+
+
+app.get("",(req, res)=>{
+    fs.readdir(pwd, (err, files)=>{
+        if(err)
+            throw err;
+        // console.log("called function executed!");
+        // console.log(filterDot(files));
+        res.json(filterDot(files))
+    });
+});
+
 
 app.listen(3000, ()=>{
     console.log("Server Running on port: 3000");
